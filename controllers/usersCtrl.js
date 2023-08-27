48861a3e073d4dd2df984001b30ede17d4c3585c
Can Cömertpay <cancomertpay@gmail.com>
2023-08-27 19:00:01 +0300

    User Authentication section | Hash user password

--- controllers/usersCtrl.js
+++ controllers/usersCtrl.js
@@ -1,5 +1,5 @@
 import User from '../model/User.js';
-
+import bcrypt from 'bcryptjs'
 // @desc Register user
 // @route POST /api/v1/users/register
 // @access Private/Admin
@@ -13,11 +13,17 @@ export const registerUserCtrl = async (req, res) => {
     res.json({
       msg: 'User already exists.'
     })
-  }
+  };
+
   // hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt)
+
   // create the user
   const user = await User.create({
-    fullname, email, password
+    fullname, 
+    email, 
+    password: hashedPassword
   });
   res.status(201).json({
     status: 'succes',
