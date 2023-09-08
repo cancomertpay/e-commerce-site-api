0d6b3aae5dd9ab0aec2d2760464959b786fb7b40
Can Cömertpay <cancomertpay@gmail.com>
2023-09-08 14:58:02 +0300

    Product Categories Controller Section | Get All, Get Single, Update, Delete Categories Controller methods

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createCategoryCtrl } from "../controllers/categoriesCtrl.js";
+import { createCategoryCtrl, getAllCategoriesCtrl, getSingleCategoryCtrl, updateCategoryCtrl, deleteCategoryCtrl } from "../controllers/categoriesCtrl.js";
 import { isLoggedIn } from "../middlewares/isLoggedin.js";
 
 const categoriesRouter = express.Router();
 
 categoriesRouter.post('/', isLoggedIn, createCategoryCtrl);
+categoriesRouter.get('/', getAllCategoriesCtrl);
+categoriesRouter.get('/:id', getSingleCategoryCtrl);
+categoriesRouter.put('/:id', isLoggedIn, updateCategoryCtrl);
+categoriesRouter.delete('/:id', isLoggedIn, deleteCategoryCtrl);
 
 export default categoriesRouter;
