dc2adc51a236b41ba411f80519809af672a2d19b
Can Cömertpay <cancomertpay@gmail.com>
2023-09-09 22:01:54 +0300

    Product Order Processing | #Update user shipping address

--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -7,6 +7,6 @@ const userRoutes = express.Router();
 userRoutes.post('/register', registerUserCtrl);
 userRoutes.post('/login', loginUserCtrl);
 userRoutes.get('/profile', isLoggedIn, getUserProfileCtrl);
-userRoutes.put('/update/shipping', isLoggedIn, updateShippingAddressCtrl);
+userRoutes.put('/update/shipping', isLoggedIn, updateShippingAddressCtrl); 
 
 export default userRoutes;
