a00470d04ae7aece33ed2a38939e686b1c9b6a33
Can Cömertpay <cancomertpay@gmail.com>
2023-09-10 16:01:19 +0300

    Stripe Payment Webhook | #Integration stripe webhook

--- app/app.js
+++ app/app.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import Stripe from 'stripe'
 dotenv.config();
 import express from 'express';
 import dbConnect from '../config/dbConnect.js';
@@ -26,6 +27,41 @@ app.use('/api/v1/brands/', brandsRouter);
 app.use('/api/v1/colors/', colorsRouter);
 app.use('/api/v1/reviews/', reviewRouter);
 app.use('/api/v1/orders/', orderRouter);
+
+// stripe webhook
+// stripe instance
+const stripe = new Stripe(process.env.STRIPE_KEY);
+
+// This is your Stripe CLI webhook secret for testing your endpoint locally.
+const endpointSecret = "whsec_1deef0d6a231810e8b236a0e005aba540d16db57ef1c28995e89a2110226f3df";
+
+app.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
+  const sig = request.headers['stripe-signature'];
+
+  let event;
+
+  try {
+    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    console.log("event");
+  } catch (err) {
+    response.status(400).send(`Webhook Error: ${err.message}`);
+    return;
+  }
+
+  // Handle the event
+  switch (event.type) {
+    case 'payment_intent.succeeded':
+      const paymentIntentSucceeded = event.data.object;
+      // Then define and call a function to handle the event payment_intent.succeeded
+      break;
+    // ... handle other event types
+    default:
+      console.log(`Unhandled event type ${event.type}`);
+  }
+
+  // Return a 200 response to acknowledge receipt of the event
+  response.send();
+});
 
 
 // err middlewares
