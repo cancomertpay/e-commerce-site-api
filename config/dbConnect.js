600113af87c067907b7fa067fcd2cb9af0bcdc8a
Can Cömertpay <cancomertpay@gmail.com>
2023-09-12 19:02:14 +0300

    error fixed v1.4

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
 const dbConnect = async () => {
   try {
     mongoose.set("strictQuery", false);
-    const connected = await mongoose.connect(process.env.MONGO_URL);
+    const connected = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Mongodb connected ${connected.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
